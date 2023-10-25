a02ed1f24107ca487966799cdb2a586dd1ed6b92
aminah_p <aminah2381@gmail.com>
2023-10-25 04:17:52 -0500

    initial commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+var fans = document.querySelector(".fans");
+var fish = document.querySelector(".fish");
+var pets = document.querySelector(".pets");
+
+fans.addEventListener("mouseover", function () {
+    fans.innerText = "123K";
+});
+
+fish.addEventListener("mouseover", function() {
+    fish.innerText = "47K";
+});
+
+pets.addEventListener("mouseover", function() {
+    pets.innerText = "20K";
+});
+
+var toggle = document.querySelector("#toggle");
+var ball = document.querySelector(".ball");
+var h1 = document.querySelector("h1");
+var body = document.querySelector("body");
+
+toggle.addEventListener ("click", function () {
+    if(body.classList.contains("light")){
+    body.classList.remove("light");
+    ball.classList.remove("move-right");
+    }else {
+        body.classList.add("light");
+        ball.classList.add("move-right");
+    }
+});
